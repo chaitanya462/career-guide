f16317436d5ad3de64ca7bc908adbd018d811acf
TUSHAR SHARMA <tushar.sharma@simplifyvms.com>
2023-08-09 16:49:48 +0530

    backend-files-deployed

---
+++ Backend/careerGuide/src/main/java/com/example/careerGuide/CareerGuideApplication.java
@@ -0,0 +1,13 @@
+package com.example.careerGuide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerGuideApplication.class, args);
+	}
+
+}
---
+++ Backend/careerGuide/src/test/java/com/example/careerGuide/CareerGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.careerGuide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
